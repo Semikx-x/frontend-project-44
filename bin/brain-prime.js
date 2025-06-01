d66615cf74d977c71b4271af29542fe1b2d18a9e
Semikx-x <semen.samsonov@mail.ru>
2025-06-01 18:04:13 +0300

    fix

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -6,7 +6,7 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
-console.log('"yes" if given number is prime. Otherwise answer "no".');
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
 
 const isPrime = (num) => {
