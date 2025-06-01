1405372b8832bd0f1a694232e65d130087f53345
Semikx-x <semen.samsonov@mail.ru>
2025-06-01 17:22:09 +0300

    gcd

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -6,23 +6,15 @@ import readlineSync from 'readline-sync';
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
-console.log('What is the result of the expression?');
+console.log('Find the greatest common divisor of given numbers');
 
-const znaki = ['+', '-', '*']
 
-const primer = (num1, random, num2) => {
-
-    if (random === 0) {
-        return num1 + num2
+const nod = (num1, num2) => {
+    if (num2 !== 0) {
+        const k = num1 % num2;
+        return nod(num2, k);
     }
-
-    if (random === 1) {
-        return num1 - num2
-    }
-
-    if (random === 2) {
-        return num1 * num2
-    }
+    return num1;
 }
 const roundsToWin = 3;
 let correctAnswers = 0;
@@ -30,11 +22,10 @@ let correctAnswers = 0;
 while (correctAnswers < roundsToWin) {
     const number1 = Math.floor(Math.random() * 100);
     const number2 = Math.floor(Math.random() * 100);
-    const randam = Math.floor(Math.random() * 3);
-    console.log(`Question: ${number1} ${znaki[randam]} ${number2}`);
+    console.log(`Question: ${number1} ${number2}`);
     const answer = readlineSync.question('Your answer: ').toLowerCase();
 
-    const correctAnswer = primer(number1, randam, number2);
+    const correctAnswer = nod(number1, number2);
 
 
     if (answer == correctAnswer) {
